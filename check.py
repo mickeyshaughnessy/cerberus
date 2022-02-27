4052f28ed8b55e014e8aee285c205af3826ec2fd
Michael Shaughnessy <michaelshaughnessy@Michaels-MacBook-Pro.local>
2022-02-27 15:50:56 -0700

    update references

--- check.py
+++ check.py
@@ -1,14 +1,31 @@
 import requests
 import os, time
 
+bench = ["www.google.com", "www.nytimes.com", "www.amazon.com", "www.president.gov.ua", "www.usa.gov"]
+bench = ["https://" + b for b in bench]
+
 targets = []
 while True:
-    with open("urls.txt") as f:
-        os.system('clear')
+    os.system('clear')
+    print("References:")
+    for b in bench:
+        try:
+            r = requests.get(b, timeout=1)
+        except:
+            r = "failed"
+        print(b + " : " + str(r))
+    print("===================")
+    with open("all_urls.txt") as f:
         for line in f:
+            line = line.rstrip()
             try:
-                r = requests.get(line.rstrip(), timeout=1)
+                r = requests.get(line, timeout=1)
+                targets.append(line)
             except:
                 r = "failed"
             print(line.rstrip() + " : " + str(r))
-        time.sleep(3)
+    with open("targets.txt", "w") as f:
+        for t in targets:
+            f.write(t+'\n')
+    print("finished checking domains, napping")
+    time.sleep(5)
