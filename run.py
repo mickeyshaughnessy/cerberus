c029bcd6533fd240ed16c911a4a25ab65a3584bb
Michael Shaughnessy <michaelshaughnessy@Michaels-MacBook-Pro.local>
2022-02-27 17:39:19 -0700

    add vegeta run script

---
+++ run.py
@@ -0,0 +1,16 @@
+import os, time
+
+RATE, WORKERS, DUR = 1000, 40, 30
+while True:
+
+    with open('targets.txt') as f:
+        for line in f:
+            url = line.rstrip()
+            os.run('
+                    echo "GET %s" |
+                    vegeta attack -rate=%s -workers=%s -duration=%ss | 
+                    tee results.bin | 
+                    vegeta report' % (url, RATE, WORKERS, DUR))
+            os.run('cat results.bin | vegeta plot > plot.html')
+            print("Finished testing %s, napping" % url)
+            time.sleep(5)
